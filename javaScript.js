aeb13e8316f5d69560382cd460c6b188176fa4cd
Andrii Zhabynets <a.zhabynets03@gmail.com>
2022-01-04 21:23:56 +0200

    create burger commit

--- javaScript.js
+++ javaScript.js
@@ -28,3 +28,19 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
+
+
+
+
+
+
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+}
